4d1198159e0a5cab9d00eabfe5a32bdf051d7e13
Rohith J <146927483+Rohithjeevanantham@users.noreply.github.com>
2024-07-21 22:02:49 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -10,7 +10,7 @@ import json
 
 # Initialize Pinecone
 # Initialize Pinecone
-pinecone_api_key = st.secrets["general"]["PINECONE_API_KEY"]
+pinecone_api_key = st.secrets["general"]["pinecone_api_key"]
 pinecone_environment = "us-east-1"
 if not pinecone_api_key:
     raise ValueError("Pinecone API key not found. Please set it in the environment variables.")
@@ -123,7 +123,7 @@ def generate_response_from_chunks(chunks, query):
         "Please provide a detailed and informative response based on the given context."
     )
     user_query = prompt_template.format(context=combined_content, query=query)
-    huggingface_token = st.secrets["general"]["HUGGINGFACE_TOKEN"]
+    huggingface_token = st.secrets["general"]["huggingface_token"]
     client = InferenceClient("meta-llama/Meta-Llama-3-8B-Instruct", token=huggingface_token)
     response = client.chat_completion(messages=[{"role": "user", "content": user_query}], max_tokens=500, stream=False)
     return response['choices'][0]['message']['content'] if response['choices'] else "No response received."
